2c8942fd62c819a80fc15bb8bc89e7f665813038
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-06-05 16:45:41 +0200

    Update esc.py

--- esc.py
+++ esc.py
@@ -1,21 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
-p = GPIO.PWM(12, 50)
 
 class Motor:
-  def __init__(self, motor):
+  def __init__(self, pin=12):
+    self.pin = pin
+    self.max_power = 8                                # Truelly 8?
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(12, GPIO.OUT)
-    self.motor = motor
+    GPIO.setup(self.pin, GPIO.OUT)
+    self.motor = GPIO.PWM(self.pin, 50)
+    print('Motor initialized on pin {}'.format(pin))
+    
   def __del__(self):
     GPIO.cleanup()
+    print('Cleaning up GPIO pins...')
+    
   def arm(self):
     self.motor.start(3)
-  def startup(self):
-          for i in range(60, 80, 1):
-                  self.motor.ChangeDutyCycle(i/10)
-                  time.sleep(0.01)
-          time.sleep(0.1)
-          self.motor.start(3)
-          time.sleep(0.75)
+    print('Motor {} ARMED'.format(self.pin))
+    
+  def change_power(self, power):
+    print('Increasing power to {}'.format(power))
+    self.motor.ChangeDutyCycle(power)
+  
+  def engine_cutoff(self):
+    print('Turning motor off...')
+    self.motor.start(3)
+    
+
