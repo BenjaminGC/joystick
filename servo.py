9e4c8225d8fc89aa9742d9fa19ef14436acef462
BenjaminGC-Mac <59574930+BenjaminGC@users.noreply.github.com>
2020-06-05 20:30:29 +0200

    Small AttributeError fixed

--- servo.py
+++ servo.py
@@ -13,8 +13,7 @@ class Servo:
         self.servo = GPIO.PWM(self.pin, 50)  # creating servo object at designated pin
 
     def __del__(self):
-        self.servo.ChangeDutyCycle(self.center)
-        time.sleep(1)
+        Servo.arm(self, level=True)
         GPIO.cleanup()
         print('Cleaning up GPIO pins...')
 
