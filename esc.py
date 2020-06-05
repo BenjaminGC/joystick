108147e551531b060fa1d5055bd7727f282d4ed5
BenjaminGC <59574930+BenjaminGC@users.noreply.github.com>
2020-06-05 18:23:12 +0200

    Adding features

--- esc.py
+++ esc.py
@@ -15,7 +15,7 @@ class Motor:
         print('Motor initialized on pin {}'.format(pin))
 
     def __del__(self):
-        Motor.engine_cutoff(self)
+        Motor.engine_cutoff(self, cutoff=True)
         GPIO.cleanup()
         print('Cleaning up GPIO pins...')
 
@@ -23,19 +23,28 @@ class Motor:
         self.motor.start(self.arm_power)
         print('Motor {} ARMED'.format(self.pin))
 
-    def change_power(self, power, runtime=0):
+    def change_power(self, power, runtime=0, auto_cutoff=False):
         if 3 <= power <= self.max_power:
             print('Increasing power to {}'.format(power))
             self.motor.ChangeDutyCycle(power)
             time.sleep(runtime)
+            if auto_cutoff:
+                Motor.engine_cutoff(self, idle=True)
         else:
             raise ValueError('Max power setting is {}, given power setting was {}'.format(self.max_power, power))
 
-    def engine_cutoff(self):
-        print('Turning motor off...')
-        self.motor.start(3)
+    def engine_cutoff(self, cutoff=False, idle=False):
+        if idle:
+            print('Back to idle')
+            self.motor.start(3)
+        elif cutoff:
+            print('Turning motor off...')
+            self.motor.start(3)
+        else:
+            print('Action not specifiec, going back to armed position')
+            Motor.arm(self)
 
 
 motor_1 = Motor(12, max_power=12)
 value = int(input('Power: '))
-motor_1.change_power(value, runtime=2)
+motor_1.change_power(value, runtime=2, auto_cutoff=True)
